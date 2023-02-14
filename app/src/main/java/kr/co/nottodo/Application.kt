df8e7da16208655a688922ce85f31eb94f37aa2b
giovannijunseokim <kingjunseo@gmail.com>
2023-02-14 18:04:38 +0900

    [feat]#4 Application에서 Shared Preferences 객체 초기화

--- app/src/main/java/kr/co/nottodo/Application.kt
+++ app/src/main/java/kr/co/nottodo/Application.kt
@@ -3,6 +3,7 @@ package kr.co.nottodo
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import kr.co.nottodo.data.local.SharedPreference
 import timber.log.Timber
 
 class Application : Application() {
@@ -10,10 +11,15 @@ class Application : Application() {
         super.onCreate()
         setupTimber()
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        setupSharedPreferences()
     }
 
     private fun setupTimber() {
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+    }
+
+    private fun setupSharedPreferences() {
+        SharedPreference.init(this)
     }
 }
