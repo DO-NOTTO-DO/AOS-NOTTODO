01b08d96815eba06505ec3777c1256433ab2cf5a
yoon <cbj0010@naver.com>
2023-02-21 03:15:24 +0900

    [feat]#12 make Home homeAdapter, Response만들고 layout 생성

---
+++ app/src/main/java/kr/co/nottodo/presentation/home/view/HomeAdpater.kt
@@ -0,0 +1,41 @@
+package kr.co.nottodo.presentation.home.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.nottodo.data.model.ResponseHomeDaily
+import kr.co.nottodo.databinding.ItemListHomeTodoBinding
+import kr.co.nottodo.util.DiffUtilItemCallback
+
+class HomeAdpater(private val itemClick: (ResponseHomeDaily) -> Unit) :
+    ListAdapter<ResponseHomeDaily, HomeAdpater.HomeViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val binding =
+            ItemListHomeTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(binding, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.onBind(currentList[position])
+    }
+
+    class HomeViewHolder(
+        private val binding: ItemListHomeTodoBinding,
+        private val itemClick: (ResponseHomeDaily) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ResponseHomeDaily) {
+            binding.tvHomeTodoSituation.text = data.situation
+            binding.tvHomeTodo.text = data.title
+            binding.ivHomeTodoCheck.setOnClickListener { absoluteAdapterPosition }
+        }
+    }
+
+    companion object {
+        val diffUtil = DiffUtilItemCallback<ResponseHomeDaily>(
+            onItemsTheSame = { old, new -> old.id == new.id },
+            onContentsTheSame = { old, new -> old == new }
+        )
+    }
+}
--- app/src/main/java/kr/co/nottodo/presentation/home/view/HomeFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/home/view/HomeFragment.kt
@@ -5,13 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kr.co.nottodo.databinding.FragmentCalendarBinding
+import kr.co.nottodo.data.model.ResponseHomeDaily
 import kr.co.nottodo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding)
+    private lateinit var homeAdpater: HomeAdpater
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +24,43 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
     }
+
+    private fun initAdapter() {
+        homeAdpater = HomeAdpater()
+        binding.rvHomeTodoList.adapter = homeAdpater
+        val todoList = listOf(
+            ResponseHomeDaily(
+                missions = 1,
+                id = 1,
+                title = "어쩌구",
+                completionStatus = "저쩌구",
+                situation = "잉"
+            ),
+            ResponseHomeDaily(
+                missions = 1,
+                id = 2,
+                title = "어쩌구2",
+                completionStatus = "저쩌구2",
+                situation = "잉"
+            ),
+            ResponseHomeDaily(
+                missions = 1,
+                id = 3,
+                title = "어쩌구3",
+                completionStatus = "저쩌구3",
+                situation = "잉"
+            )
+        )
+
+
+        homeAdpater.submitList(todoList)
+    }
+
+//    private fun menuclick(index:Int) {
+//return ind
+//    }
 
     override fun onDestroyView() {
         _binding = null
