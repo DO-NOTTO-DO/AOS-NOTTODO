288c1cdab09c9c667d7c474a33896770c6656c50
giovannijunseokim <kingjunseo@gmail.com>
2023-02-21 18:56:20 +0900

    [refactor]#8 뷰모델 코드 다이어트

--- app/src/main/java/kr/co/nottodo/presentation/addition/viewmodel/AdditionViewModel.kt
+++ app/src/main/java/kr/co/nottodo/presentation/addition/viewmodel/AdditionViewModel.kt
@@ -4,36 +4,29 @@ import androidx.lifecycle.*
 
 class AdditionViewModel : ViewModel() {
     val mission: MutableLiveData<String> = MutableLiveData()
-    val isMissionFilled: LiveData<Boolean> = Transformations.map(mission) {
+    private val isMissionFilled: LiveData<Boolean> = Transformations.map(mission) {
         it.isNotBlank()
     }
     val situation: MutableLiveData<String> = MutableLiveData()
-    val isSituationFilled: LiveData<Boolean> = Transformations.map(situation) {
+    private val isSituationFilled: LiveData<Boolean> = Transformations.map(situation) {
         it.isNotBlank()
     }
     val action: MutableLiveData<String> = MutableLiveData()
-    val isActionFilled: LiveData<Boolean> = Transformations.map(action) {
-        it.isNotBlank()
-    }
     val goal: MutableLiveData<String> = MutableLiveData()
 
     val isAbleToAdd: MediatorLiveData<Boolean> = MediatorLiveData()
 
     init {
-        isAbleToAdd.addSource(isMissionFilled){
+        isAbleToAdd.addSource(isMissionFilled) {
             isAbleToAdd.value = _isAbleToAdd()
         }
-        isAbleToAdd.addSource(isSituationFilled){
-            isAbleToAdd.value = _isAbleToAdd()
-        }
-        isAbleToAdd.addSource(isActionFilled){
+        isAbleToAdd.addSource(isSituationFilled) {
             isAbleToAdd.value = _isAbleToAdd()
         }
     }
+
     private fun _isAbleToAdd(): Boolean {
         return (isMissionFilled.value == true
-                && isMissionFilled.value == true
-                && isSituationFilled.value == true
-                && isActionFilled.value == true)
+                && isSituationFilled.value == true)
     }
 }
