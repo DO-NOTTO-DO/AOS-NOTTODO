c6b7ccd98b4f0aa7f2df16fd67f76f5b81fdacc9
giovannijunseokim <kingjunseo@gmail.com>
2023-03-02 20:50:50 +0900

    [feat]#16 카카오 토큰 얻어오는 기능 구현

--- app/src/main/java/kr/co/nottodo/presentation/login/view/LoginActivity.kt
+++ app/src/main/java/kr/co/nottodo/presentation/login/view/LoginActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import kr.co.nottodo.MainActivity
 import kr.co.nottodo.databinding.ActivityLoginBinding
 import kr.co.nottodo.presentation.login.viewmodel.LoginViewModel
+import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
@@ -20,6 +25,51 @@ class LoginActivity : AppCompatActivity() {
         binding.ivLoginLabelKakao.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+
+        setKakaoLogin()
+    }
+
+    private fun setKakaoLogin() {
+        val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Timber.e("로그인 실패", error)
+            } else if (token != null) {
+                Timber.i("로그인 성공 ${token.accessToken}")
+                Timber.i(token.accessToken)
+                // 서버에 토큰 달라고 요청
+            }
+        }
+        binding.btnLoginKakao.setOnClickListener {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                // 카카오톡 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    // 로그인 실패 부분
+                    if (error != null) {
+                        Timber.e("로그인 실패 $error")
+                        // 사용자 취소
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 다른 오류 - 카톡으로 안 될 경우
+                        else {
+                            UserApiClient.instance.loginWithKakaoAccount(
+                                this, callback = kakaoLoginCallback
+                            ) // 카카오 이메일 로그인
+                        }
+                    }
+                    // 로그인 성공 부분
+                    else if (token != null) {
+                        Timber.e("로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                Timber.d("카카오톡이 설치되어 있지 않습니다.")
+                // 카카오 이메일 로그인
+                UserApiClient.instance.loginWithKakaoAccount(
+                    this, callback = kakaoLoginCallback
+                )
+            }
         }
     }
 }
