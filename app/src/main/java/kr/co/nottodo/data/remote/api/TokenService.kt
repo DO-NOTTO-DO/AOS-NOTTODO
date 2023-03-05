eac426cf8eaddedc192636d0ba8453fc4b0297b4
giovannijunseokim <kingjunseo@gmail.com>
2023-03-05 16:36:24 +0900

    [feat]#16 토큰 서버통신 경로 수정

--- app/src/main/java/kr/co/nottodo/data/remote/api/TokenService.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/api/TokenService.kt
@@ -6,6 +6,6 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TokenService {
-    @POST
+    @POST("auth")
     suspend fun getToken(@Body request: RequestTokenDto): ResponseTokenDto
 }
