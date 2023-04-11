d22c26c0dc9f88b4ec39fffda79a9062a7b4a571
giovannijunseokim <kingjunseo@gmail.com>
2023-04-11 13:50:10 +0900

    [feat]#28 온보딩 페이지 3 구현

---
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardThirdFragment.kt
@@ -0,0 +1,38 @@
+package kr.co.nottodo.presentation.onboard.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import kr.co.nottodo.databinding.FragmentOnboardThirdBinding
+import kr.co.nottodo.presentation.onboard.adapter.OnboardPainAdapter
+import kr.co.nottodo.presentation.onboard.viewmodel.OnboardViewModel
+
+class OnboardThirdFragment : Fragment() {
+    private var _binding: FragmentOnboardThirdBinding? = null
+    private val binding: FragmentOnboardThirdBinding
+        get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentOnboardThirdBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvOnboardThird.adapter = OnboardPainAdapter(
+            requireContext(),
+            ViewModelProvider(requireActivity())[OnboardViewModel::class.java].painList,
+        )
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
