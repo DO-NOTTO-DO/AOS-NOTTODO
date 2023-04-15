28b46a076d6e84dff80499411046725dace7db57
giovannijunseokim <kingjunseo@gmail.com>
2023-04-15 16:09:43 +0900

    [feat]#28 4번째 온보딩 뷰 리사이클러 뷰 스크롤 막기 및 함수화

--- app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardFourthFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardFourthFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentOnboardFourthBinding
 import kr.co.nottodo.presentation.onboard.OnboardInterface
@@ -35,46 +36,68 @@ class OnboardFourthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+        initCompleteBtnClickListener()
+        observeIsBtnClickable()
+    }
+
+    private fun initCompleteBtnClickListener() {
+        binding.btnOnboardFourthComplete.setOnClickListener {
+            if (isCompleteBtnClickable()) {
+                onboardInterface.changeFragment(OnboardFifthFragment())
+                onboardInterface.setIndicatorNext()
+            }
+        }
+    }
+
+    private fun isCompleteBtnClickable(): Boolean {
+        return binding.btnOnboardFourthComplete.currentTextColor == getColor(
+            requireContext(), R.color.black
+        )
+    }
+
+    private fun observeIsBtnClickable() {
+        ViewModelProvider(requireActivity())[OnboardViewModel::class.java].isBtnClickable.observe(
+            viewLifecycleOwner
+        ) { isBtnClickable ->
+            if (isBtnClickable) {
+                setBtnClickable()
+            } else {
+                setBtnNonClickable()
+            }
+        }
+    }
+
+    private fun setBtnClickable() {
+        with(binding.btnOnboardFourthComplete) {
+            setTextColor(
+                getColor(requireContext(), R.color.black)
+            )
+            setBackgroundResource(R.drawable.rectangle_solid_white_radius_50)
+        }
+    }
+
+    private fun setBtnNonClickable() {
+        with(binding.btnOnboardFourthComplete) {
+            setTextColor(
+                getColor(
+                    requireContext(), R.color.gray_4_9398aa
+                )
+            )
+            setBackgroundResource(R.drawable.rectangle_solid_gray_2_radius_50)
+        }
+    }
+
+    private fun initRecyclerView() {
         binding.rvOnboardFourth.adapter = OnboardSituationAdapter(
             requireContext(),
             ViewModelProvider(requireActivity())[OnboardViewModel::class.java].situationList,
             ViewModelProvider(requireActivity())[OnboardViewModel::class.java].plusOneInSituationCount,
             ViewModelProvider(requireActivity())[OnboardViewModel::class.java].minusOneInSituationCount
         )
-        binding.btnOnboardFourthComplete.setOnClickListener {
-            if (binding.btnOnboardFourthComplete.currentTextColor == R.color.black) onboardInterface.changeFragment(
-                OnboardFifthFragment()
-            )
-        }
-        ViewModelProvider(requireActivity())[OnboardViewModel::class.java].isBtnClickable.observe(
-            viewLifecycleOwner
-        ) { isBtnClickable ->
-            if (isBtnClickable) {
-                with(binding.btnOnboardFourthComplete) {
-                    setTextColor(
-                        getColor(requireContext(), R.color.black)
-                    )
-                    setBackgroundResource(R.drawable.rectangle_solid_white_radius_50)
-                }
-            } else {
-                with(binding.btnOnboardFourthComplete) {
-                    setTextColor(
-                        getColor(
-                            requireContext(), R.color.gray_4_9398aa
-                        )
-                    )
-                    setBackgroundResource(R.drawable.rectangle_solid_gray_2_radius_50)
-                }
-            }
-        }
-        binding.btnOnboardFourthComplete.setOnClickListener {
-            if (binding.btnOnboardFourthComplete.currentTextColor == getColor(
-                    requireContext(),
-                    R.color.black
-                )
-            ) {
-                onboardInterface.changeFragment(OnboardFifthFragment())
-                onboardInterface.setIndicatorNext()
+        binding.rvOnboardFourth.layoutManager = object : GridLayoutManager(context, 2) {
+            override fun canScrollVertically(): Boolean {
+                return false
             }
         }
     }
