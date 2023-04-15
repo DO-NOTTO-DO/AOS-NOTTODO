4416749fc858b37881b3ade1bf8cc42a031b5d72
giovannijunseokim <kingjunseo@gmail.com>
2023-04-15 15:59:35 +0900

    [feat]#28 3번째 온보딩 뷰 리사이클러 뷰 스크롤 막기

--- app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardThirdFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardThirdFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.nottodo.databinding.FragmentOnboardThirdBinding
 import kr.co.nottodo.presentation.onboard.adapter.OnboardPainAdapter
 import kr.co.nottodo.presentation.onboard.viewmodel.OnboardViewModel
@@ -34,6 +35,11 @@ class OnboardThirdFragment : Fragment() {
             requireContext(),
             ViewModelProvider(requireActivity())[OnboardViewModel::class.java].painList,
         )
+        binding.rvOnboardThird.layoutManager = object : LinearLayoutManager(context) {
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
     }
 
     override fun onDestroyView() {
