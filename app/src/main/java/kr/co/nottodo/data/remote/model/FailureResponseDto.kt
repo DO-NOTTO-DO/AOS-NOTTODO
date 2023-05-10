c63df185bd3a827611ec6b7afa0c90dc957d91c8
giovannijunseokim <kingjunseo@gmail.com>
2023-05-11 02:36:03 +0900

    [feat]#32 HTTP Exception을 받기 위한 Dto 제작

---
+++ app/src/main/java/kr/co/nottodo/data/remote/model/FailureResponseDto.kt
@@ -0,0 +1,14 @@
+package kr.co.nottodo.data.remote.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FailureResponseDto(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("message")
+    val message: String,
+)
