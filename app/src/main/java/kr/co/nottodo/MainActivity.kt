16086f1cbcb5d228f59fa241a4919779c83a601a
giovannijunseokim <kingjunseo@gmail.com>
2023-05-12 15:27:31 +0900

    [feat]#42 MainActivity에서 MainInterface의 activity background color 수정 함수 구현

--- app/src/main/java/kr/co/nottodo/MainActivity.kt
+++ app/src/main/java/kr/co/nottodo/MainActivity.kt
@@ -7,11 +7,12 @@ import androidx.fragment.app.commit
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import kr.co.nottodo.databinding.ActivityMainBinding
+import kr.co.nottodo.interfaces.MainInterface
 import kr.co.nottodo.presentation.achieve.AchieveFragment
 import kr.co.nottodo.presentation.home.view.HomeFragment
 import kr.co.nottodo.presentation.mypage.view.MyPageFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainInterface {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +53,14 @@ class MainActivity : AppCompatActivity() {
     private fun changeFragment(fragment: Fragment) {
         supportFragmentManager.commit {
             replace(R.id.fcv_main, fragment)
+        }
+    }
+
+    override fun setActivityBackgroundColorBasedOnFragment(thisFragment: Fragment) {
+        when (thisFragment) {
+            is HomeFragment -> binding.root.setBackgroundColor(getColor(R.color.bg_f2f2f7))
+            is AchieveFragment -> binding.root.setBackgroundColor(getColor(R.color.black))
+            is MyPageFragment -> binding.root.setBackgroundColor(getColor(R.color.black))
         }
     }
 
