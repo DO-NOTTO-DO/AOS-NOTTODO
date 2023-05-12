9e0f16776bcb068a7c7461b099b1c85a58085426
giovannijunseokim <kingjunseo@gmail.com>
2023-05-12 21:52:57 +0900

    [feat]#42 listener pattern 사용

--- app/src/main/java/kr/co/nottodo/presentation/achieve/AchieveFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/achieve/AchieveFragment.kt
@@ -1,17 +1,25 @@
 package kr.co.nottodo.presentation.achieve
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kr.co.nottodo.databinding.FragmentAchieveBinding
-import kr.co.nottodo.interfaces.MainInterface
+import kr.co.nottodo.listeners.OnFragmentChangedListener
 
 
 class AchieveFragment : Fragment() {
     private var _binding: FragmentAchieveBinding? = null
     private val binding: FragmentAchieveBinding get() = requireNotNull(_binding)
+    private var onFragmentChangedListener: OnFragmentChangedListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onFragmentChangedListener = context as? OnFragmentChangedListener
+            ?: throw TypeCastException("context can not cast as OnFragmentChangedListener")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,12 +30,14 @@ class AchieveFragment : Fragment() {
         return binding.root
     }
 
-    private fun setActivityBackgroundColor() {
-        (context as MainInterface).setActivityBackgroundColorBasedOnFragment(this)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setActivityBackgroundColor()
+    }
+
+    private fun setActivityBackgroundColor() {
+        onFragmentChangedListener?.setActivityBackgroundColorBasedOnFragment(this@AchieveFragment)
+            ?: throw NullPointerException("onFragmentChangedListener is null")
     }
 
     override fun onDestroyView() {
@@ -35,4 +45,8 @@ class AchieveFragment : Fragment() {
         super.onDestroyView()
     }
 
+    override fun onDetach() {
+        onFragmentChangedListener = null
+        super.onDetach()
+    }
 }
