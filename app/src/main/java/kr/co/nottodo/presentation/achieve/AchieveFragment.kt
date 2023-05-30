677920b9143531e89f283de4fab52612b80fab97
yoon <cbj0010@naver.com>
2023-05-31 01:04:54 +0900

    [feat[achieve]]#61 achieve server

--- app/src/main/java/kr/co/nottodo/presentation/achieve/AchieveFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/achieve/AchieveFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import kr.co.nottodo.databinding.FragmentAchieveBinding
 import kr.co.nottodo.listeners.OnFragmentChangedListener
+import kr.co.nottodo.view.calendar.monthly.util.convertStringToDate
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
 class AchieveFragment : Fragment() {
     private var _binding: FragmentAchieveBinding? = null
     private val binding: FragmentAchieveBinding get() = requireNotNull(_binding)
     private var onFragmentChangedListener: OnFragmentChangedListener? = null
+    private val achieveViewModel by viewModels<AchieveFragmentViewModel>()
+    private var todayData = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM"))
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -33,11 +39,41 @@ class AchieveFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setActivityBackgroundColor()
+        initDay()
+        observeData()
     }
 
     private fun setActivityBackgroundColor() {
         onFragmentChangedListener?.setActivityBackgroundColorBasedOnFragment(this@AchieveFragment)
             ?: throw NullPointerException("onFragmentChangedListener is null")
+    }
+
+    private fun observeData() {
+        achieveViewModel.calenderCount.observe(viewLifecycleOwner) {
+            val notTodoRate = it?.map {
+                it.actionDate.convertStringToDate() to it.percentage
+            } ?: emptyList()
+
+            val todoList =
+                listOf<Pair<String?, Int>>(
+                    Pair("2023-05-22", 1),
+                    Pair("2023-05-15", 1),
+                    Pair("2023-05-29", 2),
+                    Pair("2023-05-08", 3)
+                )
+            val returnDate = todoList.map {
+                it.first?.convertStringToDate() to it.second
+            }
+            binding.achieveCalender.setNotToDoCountList(returnDate)
+        }
+    }
+
+    private fun initDay() {
+        achieveViewModel.getCalenderRate(todayData)
+    }
+
+    private fun createDialog() {
+//            binding.achieveCalender.setNotToDoCountList()
     }
 
     override fun onDestroyView() {
