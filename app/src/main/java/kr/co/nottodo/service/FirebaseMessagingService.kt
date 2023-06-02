f1a4033ec937c8500230d757ab3e9dfc68dcf287
giovannijunseokim <kingjunseo@gmail.com>
2023-06-02 14:58:53 +0900

    [feat]#75 onMessageReceived에서 알림 생성

--- app/src/main/java/kr/co/nottodo/service/FirebaseMessagingService.kt
+++ app/src/main/java/kr/co/nottodo/service/FirebaseMessagingService.kt
@@ -1,13 +1,12 @@
 package kr.co.nottodo.service
 
 import android.Manifest
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kr.co.nottodo.R
@@ -28,38 +27,40 @@ class FirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-
-        message.takeIf { it.data.isNotEmpty() }
         Timber.tag("fcm").e("메시지가 오긴 왔습니다. ${message.data} ${message.notification}")
 
-        val notificationManager = NotificationManagerCompat.from(
-            applicationContext
-        )
-        val builder =
-            NotificationCompat.Builder(applicationContext, getString(R.string.channel_name))
-        if (notificationManager.getNotificationChannel(getString(R.string.channel_name)) == null) {
-            val channel = NotificationChannel(
+        makePushAlarm(message)
+
+    }
+
+    private fun makePushAlarm(message: RemoteMessage) {
+
+        val builder = NotificationCompat.Builder(this, getString(R.string.channel_id))
+            .setSmallIcon(R.drawable.ic_push_alarm)
+            .setContentTitle(message.notification?.title ?: "")
+            .setContentText(message.notification?.body ?: "")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        val channel =
+            NotificationChannel(
                 getString(R.string.channel_id),
                 getString(R.string.channel_name),
                 NotificationManager.IMPORTANCE_DEFAULT
-            ).apply { description = getString(R.string.channel_desc) }
-            notificationManager.createNotificationChannel(channel)
-        }
+            ).apply {
+                description = getString(R.string.channel_desc)
+            }
 
-        val title: String = message.notification?.title ?: ""
-        val body: String = message.notification?.body ?: ""
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
 
-        builder.setContentTitle(title)
-            .setContentText(body)
-            .setSmallIcon(R.drawable.ic_launcher_background)
-
-        val notification: Notification = builder.build()
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.POST_NOTIFICATIONS
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            notificationManager.notify(1, notification)
+            notificationManager.notify(0, builder.build())
+            Timber.tag("fcm").e("알람이 만들어졌네요.")
         }
     }
 }
