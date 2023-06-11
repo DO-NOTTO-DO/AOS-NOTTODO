1c98bb8fca8eae3ff6fc8251b87d81023bb1ca07
giovannijunseokim <kingjunseo@gmail.com>
2023-06-11 22:36:23 +0900

    [feat]#84 retrofit 객체 하나로 줄임

--- app/src/main/java/kr/co/nottodo/data/remote/api/ApiFactory.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/api/ApiFactory.kt
@@ -1,6 +1,5 @@
 package kr.co.nottodo.data.remote.api
 
-import RecommendationMainListDTO
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import kr.co.nottodo.BuildConfig
@@ -23,40 +22,21 @@ object ApiFactory {
     private val client by lazy {
         OkHttpClient.Builder().addInterceptor(TokenInterceptor())
             .addInterceptor(HttpLoggingInterceptor().apply {
-                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
-            }).authenticator(TokenAuthenticator())
-            .build()
-    }
-
-    val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .client(client)
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-            .build()
-    }
-
-    private val clientForLogin by lazy {
-        OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+                level =
+                    if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
             }).build()
     }
 
-    val retrofitForSocialLogin: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL)
-            .client(clientForLogin)
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-            .build()
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType())).build()
     }
 
     inline fun <reified T> create(): T = retrofit.create(T::class.java)
-    inline fun <reified T> createForToken(): T = retrofitForSocialLogin.create(T::class.java)
 }
 
 object ServicePool {
-    val tokenService = ApiFactory.createForToken<TokenService>()
+    val tokenService = ApiFactory.create<TokenService>()
     val additionService = ApiFactory.create<AdditionService>()
     val homeService = ApiFactory.create<HomeService>()
     val achieveService = ApiFactory.create<AchieveService>()
