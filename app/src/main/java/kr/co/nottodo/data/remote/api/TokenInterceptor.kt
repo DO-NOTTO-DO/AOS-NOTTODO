95a97a8f50bfdbf83597c5af65a57e1daa3eee8d
giovannijunseokim <kingjunseo@gmail.com>
2023-06-11 23:58:18 +0900

    [feat]#84 Token Expired 시 OnTokenExpiredListener의 onTokenExpired 메서드 실행

--- app/src/main/java/kr/co/nottodo/data/remote/api/TokenInterceptor.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/api/TokenInterceptor.kt
@@ -1,20 +1,26 @@
 package kr.co.nottodo.data.remote.api
 
 import kr.co.nottodo.data.local.SharedPreferences
+import kr.co.nottodo.listeners.OnTokenExpiredListener
 import kr.co.nottodo.presentation.login.view.LoginActivity.Companion.USER_TOKEN
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class TokenInterceptor : Interceptor {
+class TokenInterceptor(private val onTokenExpiredListener: OnTokenExpiredListener) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val userToken = SharedPreferences.getString(USER_TOKEN)
+
         val originalRequest = chain.request()
         val newRequest = originalRequest.newBuilder()
-        val userToken = SharedPreferences.getString(USER_TOKEN)
         if (userToken != null) {
             newRequest.header(
                 "Authorization", userToken
             )
         }
-        return chain.proceed(newRequest.build())
+        val response = chain.proceed(newRequest.build())
+        if (response.code == 401)
+            onTokenExpiredListener.onTokenExpired()
+
+        return response
     }
 }
