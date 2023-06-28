1f3dc25113cd530a337850146fd8038afdb4c16c
giovannijunseokim <kingjunseo@gmail.com>
2023-06-28 16:04:50 +0900

    [feat]#104 서버에서 주는 message 추출을 위한 ThrowableExtension 추가

---
+++ app/src/main/java/kr/co/nottodo/util/ThrowableExtension.kt
@@ -0,0 +1,22 @@
+package kr.co.nottodo.util
+
+import kotlinx.serialization.json.Json
+import kr.co.nottodo.data.remote.model.FailureResponseDto
+import retrofit2.HttpException
+import timber.log.Timber
+
+fun Throwable.getErrorMessage(): String {
+    when (this) {
+        is HttpException -> {
+            val data = Json.decodeFromString<FailureResponseDto>(
+                this.response()?.errorBody()?.string() ?: return "예기치 못한 에러 발생"
+            )
+            return data.message
+        }
+
+        else -> {
+            Timber.e(this.message.toString())
+            return "예기치 못한 에러 발생"
+        }
+    }
+}
