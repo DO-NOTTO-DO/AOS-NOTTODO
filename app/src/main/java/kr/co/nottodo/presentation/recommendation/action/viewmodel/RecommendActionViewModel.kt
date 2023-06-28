18800f825099f3cd08224fa53c66b2e2de7bac9a
giovannijunseokim <kingjunseo@gmail.com>
2023-06-28 16:05:07 +0900

    [feat]#104 RecommendActionViewModel init

---
+++ app/src/main/java/kr/co/nottodo/presentation/recommendation/action/viewmodel/RecommendActionViewModel.kt
@@ -0,0 +1,35 @@
+package kr.co.nottodo.presentation.recommendation.action.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kr.co.nottodo.data.remote.api.ServicePool.recommendActionListService
+import kr.co.nottodo.data.remote.model.recommendation.action.ResponseRecommendActionListDTO.Action
+
+class RecommendActionViewModel : ViewModel() {
+    private var missionId: Int? = null
+    fun setMissionId(_missionId: Int) {
+        missionId = _missionId
+    }
+
+    private val _recommendActionListSuccessResponse: MutableLiveData<List<Action>> =
+        MutableLiveData()
+    val recommendActionListSuccessResponse: LiveData<List<Action>>
+        get() = _recommendActionListSuccessResponse
+
+    private val _recommendActionListErrorResponse: MutableLiveData<String> = MutableLiveData()
+    val recommendActionListErrorResponse: LiveData<String>
+        get() = _recommendActionListErrorResponse
+
+    fun getRecommendActionList() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                recommendActionListService.getRecommendActionList(missionId ?: return@launch)
+            }.fold(onSuccess = { response ->
+                _recommendActionListSuccessResponse.value = response.data
+            }, onFailure = { error -> _recommendActionListErrorResponse.value = error.message })
+        }
+    }
+}
