25c319868f5145943de29a500ff00eaa26692293
giovannijunseokim <kingjunseo@gmail.com>
2023-06-29 17:05:42 +0900

    [chore]#104 rename

--- app/src/main/java/kr/co/nottodo/data/remote/model/recommendation/action/RecommendActionListDTO.kt
+++ app/src/main/java/kr/co/nottodo/data/remote/model/recommendation/action/RecommendActionListDTO.kt
@@ -8,13 +8,13 @@ data class ResponseRecommendActionListDTO(
     @SerialName("status") val status: Int,
     @SerialName("success") val success: Boolean,
     @SerialName("message") val message: String,
-    @SerialName("data") val data: List<Action>,
+    @SerialName("data") val data: Mission,
 ) {
     @Serializable
-    data class Action(
+    data class Mission(
         @SerialName("id") val id: Int,
         @SerialName("title") val title: String,
-        @SerialName("recommendActions") val recommendActions: Action,
+        @SerialName("recommendActions") val recommendActions: List<Action>,
     ) {
         @Serializable
         data class Action(
