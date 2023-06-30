e76920fa001434cb219fe103f13a935aeaa871e0
giovannijunseokim <kingjunseo@gmail.com>
2023-06-30 17:35:36 +0900

    [feat]#118 커스텀 스낵바 클래스 생성

---
+++ app/src/main/java/kr/co/nottodo/view/snackbar/NotTodoSnackbar.kt
@@ -0,0 +1,38 @@
+package kr.co.nottodo.view.snackbar
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import kr.co.nottodo.databinding.ViewNottodoSnackbarBinding
+
+class NotTodoSnackbar(view: View, private val message: String) {
+    private val context = view.context
+    private val snackbar = Snackbar.make(view, message, 2000)
+    private val snackbarLayout = snackbar.view as Snackbar.SnackbarLayout
+
+    private val inflater = LayoutInflater.from(context)
+    private val binding: ViewNottodoSnackbarBinding =
+        ViewNottodoSnackbarBinding.inflate(inflater, null, false)
+
+    init {
+        initView()
+        initData()
+    }
+
+    private fun initView() {
+        with(snackbarLayout) {
+            removeAllViews()
+            setBackgroundColor(ContextCompat.getColor(context, android.R.color.transparent))
+            addView(binding.root, 0)
+        }
+    }
+
+    private fun initData() {
+        binding.tvNottodoSnackbarDesc.text = message
+    }
+
+    fun show() {
+        snackbar.show()
+    }
+}
