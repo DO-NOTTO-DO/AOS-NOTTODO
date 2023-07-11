7d2b423b747b482aee982f2c88abfb1b0d1ce356
giovannijunseokim <kingjunseo@gmail.com>
2023-07-12 01:35:27 +0900

    [ui]#140 상황 터치 영역 수정

--- app/src/main/java/kr/co/nottodo/presentation/onboard/adapter/OnboardSituationAdapter.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/adapter/OnboardSituationAdapter.kt
@@ -2,19 +2,16 @@ package kr.co.nottodo.presentation.onboard.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kr.co.nottodo.R
 import kr.co.nottodo.databinding.ItemOnboardFourthBinding
 
 class OnboardSituationAdapter(
     private val context: Context,
     private val itemList: List<String>,
-    private val plusSituationCount: () -> Unit,
-    private val minusSituationCount: () -> Unit,
-) :
-    RecyclerView.Adapter<OnboardSituationAdapter.OnboardSituationViewHolder>() {
+    private val addSituation: (String) -> Unit,
+    private val removeSituation: (String) -> Unit,
+) : RecyclerView.Adapter<OnboardSituationAdapter.OnboardSituationViewHolder>() {
     lateinit var binding: ItemOnboardFourthBinding
     private val inflater by lazy { LayoutInflater.from(context) }
 
@@ -28,38 +25,31 @@ class OnboardSituationAdapter(
     }
 
     override fun onBindViewHolder(holder: OnboardSituationViewHolder, position: Int) {
-        holder.onBind(binding, position, plusSituationCount, minusSituationCount)
+        holder.onBind(binding, position, addSituation, removeSituation)
     }
 
     class OnboardSituationViewHolder(
         binding: ItemOnboardFourthBinding,
         private val itemList: List<String>,
     ) : RecyclerView.ViewHolder(binding.root) {
-
         fun onBind(
             binding: ItemOnboardFourthBinding,
             position: Int,
-            plusSituationCount: () -> Unit,
-            minusSituationCount: () -> Unit,
+            addSituation: (String) -> Unit,
+            removeSituation: (String) -> Unit,
         ) {
             with(binding) {
                 tvItemOnboardFourth.text = itemList[position]
                 tvItemOnboardFourth.setOnClickListener {
-                    if (isClicked(tvItemOnboardFourth)) {
-                        tvItemOnboardFourth.tooltipText = "unclicked"
-                        layoutItemOnboardFourth.setBackgroundResource(R.drawable.rectangle_solid_gray_1_radius_10)
-                        minusSituationCount.invoke()
+                    if (layoutItemOnboardFourth.isSelected) {
+                        layoutItemOnboardFourth.isSelected = false
+                        removeSituation.invoke(tvItemOnboardFourth.text.toString())
                     } else {
-                        tvItemOnboardFourth.tooltipText = "clicked"
-                        layoutItemOnboardFourth.setBackgroundResource(R.drawable.rectangle_solid_gray_1_stroke_green1_1_radius_10)
-                        plusSituationCount.invoke()
+                        layoutItemOnboardFourth.isSelected = true
+                        addSituation.invoke(tvItemOnboardFourth.text.toString())
                     }
                 }
             }
-        }
-
-        private fun isClicked(view: View): Boolean {
-            return view.tooltipText != "unclicked"
         }
     }
 }
