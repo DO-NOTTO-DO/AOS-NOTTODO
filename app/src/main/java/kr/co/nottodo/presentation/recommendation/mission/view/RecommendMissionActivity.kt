1876f995da7e22fa09e149ac534c8f6e1d4f3568
giovannijunseokim <kingjunseo@gmail.com>
2023-07-12 16:13:14 +0900

    [feat]#140 RecommendMissionActivity Amplitude 적용

--- app/src/main/java/kr/co/nottodo/presentation/recommendation/mission/view/RecommendMissionActivity.kt
+++ app/src/main/java/kr/co/nottodo/presentation/recommendation/mission/view/RecommendMissionActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import kr.co.nottodo.R
 import kr.co.nottodo.databinding.ActivityRecommendMissionBinding
 import kr.co.nottodo.presentation.addition.view.AdditionActivity
 import kr.co.nottodo.presentation.recommendation.action.view.RecommendActionActivity
 import kr.co.nottodo.presentation.recommendation.mission.adapter.RecommendMissionAdapter
 import kr.co.nottodo.presentation.recommendation.mission.viewmodel.RecommendMissionViewModel
 import kr.co.nottodo.presentation.recommendation.model.RecommendMissionUiModel
+import kr.co.nottodo.util.NotTodoAmplitude.trackEvent
+import kr.co.nottodo.util.NotTodoAmplitude.trackEventWithProperty
 import kr.co.nottodo.util.showToast
 
 
@@ -20,7 +23,7 @@ class RecommendMissionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        trackEvent(getString(R.string.view_recommend_mission))
         setData()
         setViews()
         setClickEvents()
@@ -56,6 +59,7 @@ class RecommendMissionActivity : AppCompatActivity() {
 
     private fun setWriteDirectlyBtnClickEvent() {
         binding.fabRecommendMissionWriteDirectly.setOnClickListener {
+            trackEvent(getString(R.string.click_self_create_mission))
             startActivity(Intent(this, AdditionActivity::class.java))
             if (!isFinishing) finish()
         }
@@ -83,6 +87,14 @@ class RecommendMissionActivity : AppCompatActivity() {
     private fun setAdapter() {
         val startRecommendActionActivity =
             { id: Int, title: String, situation: String, image: String ->
+                trackEventWithProperty(
+                    getString(R.string.click_recommend_mission),
+                    getString(R.string.situation),
+                    situation
+                )
+                trackEventWithProperty(
+                    getString(R.string.click_recommend_mission), getString(R.string.title), title
+                )
                 startActivity(
                     Intent(this, RecommendActionActivity::class.java).putExtra(
                         MISSION_DETAIL, RecommendMissionUiModel(id, title, situation, image)
