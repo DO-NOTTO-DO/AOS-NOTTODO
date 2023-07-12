1a1ef7ee16f01e2e74fded495bbb1935bcb7e263
giovannijunseokim <kingjunseo@gmail.com>
2023-07-12 22:43:48 +0900

    [feat]#140 AdditionActivity Amplitude 적용

--- app/src/main/java/kr/co/nottodo/util/FlexboxExtension.kt
+++ app/src/main/java/kr/co/nottodo/util/FlexboxExtension.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import com.google.android.flexbox.FlexboxLayout
 import kr.co.nottodo.R
+import kr.co.nottodo.util.NotTodoAmplitude.trackEventWithProperty
 
 fun FlexboxLayout.addButtons(texts: List<String>, editText: EditText) {
     for (element in texts) {
@@ -17,11 +18,15 @@ fun FlexboxLayout.addButtons(texts: List<String>, editText: EditText) {
             editText.requestFocus()
             editText.setSelection(editText.length())
             it.context.showKeyboard(editText)
+            trackEventWithProperty(
+                context.getString(R.string.click_recommend_situation),
+                context.getString(R.string.situation),
+                element
+            )
         }
 
         val layoutParams = ViewGroup.MarginLayoutParams(
-            ViewGroup.MarginLayoutParams.WRAP_CONTENT,
-            ViewGroup.MarginLayoutParams.WRAP_CONTENT
+            ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT
         )
         layoutParams.marginEnd = context.dpToPx(5)
         layoutParams.bottomMargin = context.dpToPx(8)
