3651fe2564814e0ff763430a0741162fb5eca383
giovannijunseokim <kingjunseo@gmail.com>
2023-08-16 00:59:50 +0900

    [chore]#170 인터넷 연결 안 될 시 텍스트 변경

--- app/src/main/java/kr/co/nottodo/util/PublicString.kt
+++ app/src/main/java/kr/co/nottodo/util/PublicString.kt
@@ -1,5 +1,5 @@
 package kr.co.nottodo.util
 
 object PublicString {
-    const val NO_INTERNET_CONDITION_ERROR = "NO_INTERNET_CONDITION"
+    const val NO_INTERNET_CONDITION_ERROR = "네트워크에 연결할 수 없습니다. 확인 후 다시 시도해 주세요."
 }
