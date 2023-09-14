5a4f08c87d0b7cf7dfa199ef1e65908880cd8503
giovannijunseokim <kingjunseo@gmail.com>
2023-09-14 15:23:21 +0900

    [feat]#26 LiveData, MutableLiveData 각각 오버로딩

--- app/src/main/java/kr/co/nottodo/util/LiveDataExtension.kt
+++ app/src/main/java/kr/co/nottodo/util/LiveDataExtension.kt
@@ -1,9 +1,19 @@
 package kr.co.nottodo.util
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 
 // MediatorLiveData
+fun <T> MediatorLiveData<T>.addSourceList(
+    vararg liveDataArgument: LiveData<*>,
+    onChanged: () -> T,
+) {
+    liveDataArgument.forEach {
+        this.addSource(it) { value = onChanged() }
+    }
+}
+
 fun <T> MediatorLiveData<T>.addSourceList(
     vararg liveDataArgument: MutableLiveData<*>,
     onChanged: () -> T,
