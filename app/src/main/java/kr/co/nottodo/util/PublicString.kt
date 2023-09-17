e41f236bc7e6237af3243001349889910647608a
giovannijunseokim <kingjunseo@gmail.com>
2023-09-17 17:38:30 +0900

    [chore]#26 String 리소스 통합

--- app/src/main/java/kr/co/nottodo/util/PublicString.kt
+++ app/src/main/java/kr/co/nottodo/util/PublicString.kt
@@ -5,6 +5,5 @@ object PublicString {
     const val NO_INTERNET_CONDITION_ERROR = "네트워크에 연결할 수 없습니다. 확인 후 다시 시도해 주세요."
 
     // Addition
-    const val MAX_MISSION_COUNT = "/20"
-    const val MAX_ACTION_COUNT = "/20"
+    const val MAX_COUNT_20 = "/20"
 }
