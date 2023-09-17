920815fc8be25d8592c4890e7e73045b9ca0afe9
giovannijunseokim <kingjunseo@gmail.com>
2023-09-17 12:47:56 +0900

    [chore]#26 Public String 추가

--- app/src/main/java/kr/co/nottodo/util/PublicString.kt
+++ app/src/main/java/kr/co/nottodo/util/PublicString.kt
@@ -1,5 +1,9 @@
 package kr.co.nottodo.util
 
 object PublicString {
+    // Network
     const val NO_INTERNET_CONDITION_ERROR = "네트워크에 연결할 수 없습니다. 확인 후 다시 시도해 주세요."
+
+    // Addition
+    const val MAX_MISSION_COUNT = "/20"
 }
