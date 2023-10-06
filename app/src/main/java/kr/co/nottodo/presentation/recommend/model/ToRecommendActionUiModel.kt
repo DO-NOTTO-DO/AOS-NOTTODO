d2fc59a36e35f51d6be4cff7dd6887acb579c606
giovannijunseokim <kingjunseo@gmail.com>
2023-10-06 11:10:47 +0900

    [chore]#196 UiModel rename

--- app/src/main/java/kr/co/nottodo/presentation/recommend/model/ToRecommendActionUiModel.kt
+++ app/src/main/java/kr/co/nottodo/presentation/recommend/model/ToRecommendActionUiModel.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class RecommendMissionUiModel(
+data class ToRecommendActionUiModel(
     val id: Int,
     val title: String,
     val situation: String,
