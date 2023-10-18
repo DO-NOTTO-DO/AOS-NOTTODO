fc91c130f8eeeebf5488d61409382d058191edc3
giovannijunseokim <kingjunseo@gmail.com>
2023-10-19 00:32:11 +0900

    [feat]#205 회원탈퇴 결과 옵저버 WithdrawalDialogFragment로 넘김

--- app/src/main/java/kr/co/nottodo/presentation/mypage/view/MyPageInformationFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/mypage/view/MyPageInformationFragment.kt
@@ -14,24 +14,18 @@ import androidx.navigation.fragment.findNavController
 import kr.co.nottodo.R
 import kr.co.nottodo.data.local.SharedPreferences
 import kr.co.nottodo.databinding.FragmentMyPageInformationBinding
-import kr.co.nottodo.listeners.OnDialogDismissListener
 import kr.co.nottodo.presentation.base.fragment.DataBindingFragment
 import kr.co.nottodo.presentation.login.view.LoginActivity
 import kr.co.nottodo.presentation.login.view.LoginActivity.Companion.DID_USER_CHOOSE_TO_BE_NOTIFIED
-import kr.co.nottodo.presentation.mypage.viewmodel.MyPageInformationViewModel
+import kr.co.nottodo.presentation.mypage.viewmodel.MyPageInformationNewViewModel
 import kr.co.nottodo.util.NotTodoAmplitude.trackEvent
-import kr.co.nottodo.util.PublicString.NO_INTERNET_CONDITION_ERROR
-import kr.co.nottodo.util.showNotTodoSnackBar
-import kr.co.nottodo.util.showToast
 
 
 class MyPageInformationFragment :
-    DataBindingFragment<FragmentMyPageInformationBinding>(R.layout.fragment_my_page_information),
-    OnDialogDismissListener {
+    DataBindingFragment<FragmentMyPageInformationBinding>(R.layout.fragment_my_page_information) {
 
-    private val viewModel: MyPageInformationViewModel by viewModels()
+    private val viewModel: MyPageInformationNewViewModel by viewModels()
     private val withdrawalDialogFragment by lazy { WithdrawalDialogFragment.newInstance() }
-    private val withdrawalFeedbackDialogFragment by lazy { WithdrawalFeedbackDialogFragment.newInstance() }
     private val myPageLogoutDialogFragment by lazy { MyPageLogoutDialogFragment.newInstance() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,7 +34,6 @@ class MyPageInformationFragment :
         trackEnterMyPageInformation()
         setViews()
         setClickEvents()
-        setObservers()
     }
 
     private fun trackEnterMyPageInformation() {
@@ -164,45 +157,6 @@ class MyPageInformationFragment :
         binding.ivMyPageInformationBackArrow.setOnClickListener {
             findNavController().popBackStack()
         }
-    }
-
-    private fun setObservers() {
-        setWithdrawalObserver()
-    }
-
-    private fun setWithdrawalObserver() {
-        setWithdrawalSuccessObserver()
-        setWithdrawalErrorObserver()
-    }
-
-    private fun setWithdrawalErrorObserver() {
-        viewModel.withdrawalErrorResponse.observe(viewLifecycleOwner) { errorMessage ->
-            if (errorMessage == NO_INTERNET_CONDITION_ERROR) requireContext().showNotTodoSnackBar(
-                binding.root, NO_INTERNET_CONDITION_ERROR
-            )
-            else requireContext().showToast(errorMessage)
-        }
-    }
-
-    private fun setWithdrawalSuccessObserver() {
-        viewModel.withdrawalSuccessResponse.observe(viewLifecycleOwner) {
-            withdrawalDialogFragment.dismiss()
-            withdrawalFeedbackDialogFragment.show(
-                requireActivity().supportFragmentManager, withdrawalFeedbackDialogFragment.tag
-            )
-            trackEvent(getString(R.string.complete_withdrawal))
-        }
-    }
-
-
-    override fun onDialogDismiss() {
-        logout()
-    }
-
-    private fun logout() {
-        SharedPreferences.clearForLogout()
-        startActivity(Intent(requireContext(), LoginActivity::class.java))
-        if (!requireActivity().isFinishing) requireActivity().finish()
     }
 
     override fun bindViewModelWithBinding() {
--- app/src/main/java/kr/co/nottodo/presentation/mypage/view/WithdrawalDialogFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/mypage/view/WithdrawalDialogFragment.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentWithdrawalDialogBinding
-import kr.co.nottodo.presentation.mypage.viewmodel.MyPageInformationViewModel
+import kr.co.nottodo.presentation.mypage.viewmodel.WithdrawalDialogViewModel
 import kr.co.nottodo.util.NotTodoAmplitude.trackEvent
+import kr.co.nottodo.util.PublicString
+import kr.co.nottodo.util.showNotTodoSnackBar
+import kr.co.nottodo.util.showToast
 
 class WithdrawalDialogFragment : DialogFragment() {
     private var _binding: FragmentWithdrawalDialogBinding? = null
     private val binding get() = requireNotNull(_binding)
-    private val activityViewModel by activityViewModels<MyPageInformationViewModel>()
+    private val viewModel: WithdrawalDialogViewModel by viewModels()
+    private val withdrawalFeedbackDialogFragment by lazy { WithdrawalFeedbackDialogFragment.newInstance() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +31,9 @@ class WithdrawalDialogFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setClickEvents()
         trackEvent(getString(R.string.appear_withdrawal_modal))
+        setClickEvents()
+        setObservers()
     }
 
     private fun setClickEvents() {
@@ -38,7 +43,7 @@ class WithdrawalDialogFragment : DialogFragment() {
 
     private fun setWithdrawalTvClickEvent() {
         binding.tvWithdrawal.setOnClickListener {
-            activityViewModel.withdrawal()
+            viewModel.withdrawal()
         }
     }
 
@@ -48,9 +53,37 @@ class WithdrawalDialogFragment : DialogFragment() {
         }
     }
 
+    private fun setObservers() {
+        setWithdrawalObserver()
+    }
+
+    private fun setWithdrawalObserver() {
+        setWithdrawalSuccessObserver()
+        setWithdrawalErrorObserver()
+    }
+
+    private fun setWithdrawalErrorObserver() {
+        viewModel.withdrawalErrorResponse.observe(viewLifecycleOwner) { errorMessage ->
+            if (errorMessage == PublicString.NO_INTERNET_CONDITION_ERROR) requireContext().showNotTodoSnackBar(
+                binding.root, PublicString.NO_INTERNET_CONDITION_ERROR
+            )
+            else requireContext().showToast(errorMessage)
+        }
+    }
+
+    private fun setWithdrawalSuccessObserver() {
+        viewModel.withdrawalSuccessResponse.observe(viewLifecycleOwner) {
+            withdrawalFeedbackDialogFragment.show(
+                requireActivity().supportFragmentManager, withdrawalFeedbackDialogFragment.tag
+            )
+            trackEvent(getString(R.string.complete_withdrawal))
+            dismiss()
+        }
+    }
+
     override fun onDestroyView() {
-        super.onDestroyView()
         _binding = null
+        super.onDestroyView()
     }
 
     companion object {
