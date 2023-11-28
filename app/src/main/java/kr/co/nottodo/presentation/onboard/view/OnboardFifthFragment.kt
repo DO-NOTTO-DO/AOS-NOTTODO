f60841b8189e6e1ea8707a55beea6aab10e5d4da
giovannijunseokim <kingjunseo@gmail.com>
2023-11-28 17:18:10 +0900

    [feat]#197 OnboardFifthFragment ViewBindingFragment 사용 및 navigation 연결

--- app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardFifthFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/onboard/view/OnboardFifthFragment.kt
@@ -1,43 +1,21 @@
 package kr.co.nottodo.presentation.onboard.view
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kr.co.nottodo.R
 import kr.co.nottodo.databinding.FragmentOnboardFifthBinding
-import kr.co.nottodo.presentation.onboard.OnboardInterface
+import kr.co.nottodo.presentation.base.fragment.ViewBindingFragment
 import kr.co.nottodo.presentation.onboard.adapter.OnboardMissionAdapter
 import kr.co.nottodo.presentation.onboard.viewmodel.OnboardViewModel
 import kr.co.nottodo.util.NotTodoAmplitude.trackEvent
 
-class OnboardFifthFragment : Fragment() {
-    private var _binding: FragmentOnboardFifthBinding? = null
-    private val binding: FragmentOnboardFifthBinding
-        get() = requireNotNull(_binding)
-    private lateinit var onboardInterface: OnboardInterface
-    private val viewModel by activityViewModels<OnboardViewModel>()
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        initInterface()
-    }
-
-    private fun initInterface() {
-        onboardInterface = context as OnboardInterface
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentOnboardFifthBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+class OnboardFifthFragment : ViewBindingFragment<FragmentOnboardFifthBinding>() {
+    private val viewModel by viewModels<OnboardViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,7 +26,7 @@ class OnboardFifthFragment : Fragment() {
 
     private fun initNextLayoutClickListener() {
         binding.tvOnboardFifthNext.setOnClickListener {
-            onboardInterface.changeFragment(OnboardSixthFragment())
+            findNavController().navigate(R.id.action_onboardFifthFragment_to_onboardSixthFragment)
             trackEvent(getString(R.string.click_onboarding_next_4))
         }
     }
@@ -64,8 +42,8 @@ class OnboardFifthFragment : Fragment() {
         }
     }
 
-    override fun onDestroyView() {
-        _binding = null
-        super.onDestroyView()
-    }
+    override fun setBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+    ): FragmentOnboardFifthBinding = FragmentOnboardFifthBinding.inflate(inflater, container, false)
 }
