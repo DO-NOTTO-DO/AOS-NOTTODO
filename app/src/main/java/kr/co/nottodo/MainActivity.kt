f91a5110706cb12d9c27acdf79b4625256be1fa7
yoon <cbj0010@naver.com>
2023-12-30 00:22:23 +0900

    [fix] package name불러오기

--- app/src/main/java/kr/co/nottodo/MainActivity.kt
+++ app/src/main/java/kr/co/nottodo/MainActivity.kt
@@ -1,6 +1,6 @@
 package kr.co.nottodo
 
-import android.content.ContextWrapper
+import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -124,7 +124,7 @@ class MainActivity :
     }
 
     private fun openUpdatePage() {
-        navigateToGooglePlayStore(ContextWrapper.p)
+        navigateToGooglePlayStore(this.packageName)
     }
 
     private fun setBottomNavigationView() {
