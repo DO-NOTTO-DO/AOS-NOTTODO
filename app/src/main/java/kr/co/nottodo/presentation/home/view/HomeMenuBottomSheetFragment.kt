cd7cc8f96c214c3bbc35534394578825114760f6
giovannijunseokim <kingjunseo@gmail.com>
2024-01-29 18:07:41 +0900

    [chore]#224 인자명은 소문자로 시작

--- app/src/main/java/kr/co/nottodo/presentation/home/view/HomeMenuBottomSheetFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/home/view/HomeMenuBottomSheetFragment.kt
@@ -99,7 +99,7 @@ class HomeMenuBottomSheetFragment : BottomSheetDialogFragment(), DialogCloseList
         trackEvent(getString(R.string.close_detail_mission))
     }
 
-    private fun trackListData(EventName: Int) {
+    private fun trackListData(eventName: Int) {
         val trackList = mutableMapOf<String, CharSequence>().apply {
             put(getString(R.string.title), binding.tvHomeDialogNotodo.text)
             put(getString(R.string.situation), binding.tvHomeDialogSituation.text)
@@ -107,7 +107,7 @@ class HomeMenuBottomSheetFragment : BottomSheetDialogFragment(), DialogCloseList
             val actionCharSequence: CharSequence = trackActions.joinToString(",")
             put(getString(R.string.action), actionCharSequence)
         }
-        trackEventWithPropertyList(getString(EventName), trackList)
+        trackEventWithPropertyList(getString(eventName), trackList)
     }
 
     private fun showDeleteDialog() {
