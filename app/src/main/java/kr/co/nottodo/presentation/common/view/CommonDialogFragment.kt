9a0d4ba9ab1da4ea39118e77bb5ef3c69ecd6f00
giovannijunseokim <kingjunseo@gmail.com>
2024-02-14 00:31:42 +0900

    [feat]#248 Spannable 구현 및 기능 구현

--- app/src/main/java/kr/co/nottodo/presentation/common/view/CommonDialogFragment.kt
+++ app/src/main/java/kr/co/nottodo/presentation/common/view/CommonDialogFragment.kt
@@ -3,6 +3,9 @@ package kr.co.nottodo.presentation.common.view
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.BackgroundColorSpan
 import android.view.View
 import androidx.fragment.app.viewModels
 import kr.co.nottodo.R
@@ -10,9 +13,9 @@ import kr.co.nottodo.data.local.SharedPreferences
 import kr.co.nottodo.databinding.FragmentCommonDialogBinding
 import kr.co.nottodo.presentation.base.fragment.DataBindingDialogFragment
 import kr.co.nottodo.presentation.common.viewmodel.CommonDialogViewModel
+import kr.co.nottodo.presentation.common.viewmodel.CommonDialogViewModel.EventType
 import kr.co.nottodo.util.NotTodoAmplitude
 import kr.co.nottodo.util.PublicString.STOP_WATCHING_COMMON_DIALOG
-import kr.co.nottodo.util.observeEvent
 
 class CommonDialogFragment :
     DataBindingDialogFragment<FragmentCommonDialogBinding>(R.layout.fragment_common_dialog) {
@@ -27,26 +30,42 @@ class CommonDialogFragment :
 
     private fun setView() {
         setDialogRounded()
+        setTextSpannable()
     }
 
     private fun setDialogRounded() {
         dialog?.window?.setBackgroundDrawableResource(R.drawable.rectangle_radius_20)
     }
 
-    private fun handleClickEvents() {
-        handleClickBtnEvent()
-        handleClickDismissTvEvent()
+    private fun setTextSpannable() {
+        SpannableStringBuilder(getString(R.string.common_dialog_title)).apply {
+            setSpan(
+                BackgroundColorSpan(requireContext().getColor(R.color.green_2_4ae59b)),
+                7,
+                12,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+        }.also { spannedString -> binding.tvCommonDialogTitle.text = spannedString }
     }
 
-    private fun handleClickDismissTvEvent() {
-        viewModel.dismissTvClickEvent.observeEvent(viewLifecycleOwner) {
+    private fun handleClickEvents() {
+        viewModel.event.observe(viewLifecycleOwner) {
             checkIsCheckBoxChecked()
+            when (it) {
+                EventType.DISMISS -> {
+                    NotTodoAmplitude.trackEventWithProperty(
+                        eventName = getString(R.string.click_ad_modal_close),
+                        propertyName = getString(R.string.again),
+                        propertyValue = getString(if (viewModel.isCheckBoxChecked.value == true) R.string.yes_eng else R.string.no_eng),
+                    )
+                }
+
+                EventType.NAVIGATE -> {
+                    navigateToOpenChat()
+                    NotTodoAmplitude.trackEvent(eventName = getString(R.string.click_ad_modal_cta))
+                }
+            }
             dismiss()
-            NotTodoAmplitude.trackEventWithProperty(
-                eventName = getString(R.string.click_ad_modal_close),
-                propertyName = getString(R.string.again),
-                propertyValue = getString(if (viewModel.isCheckBoxChecked.value == true) R.string.yes_eng else R.string.no_eng),
-            )
         }
     }
 
@@ -56,15 +75,8 @@ class CommonDialogFragment :
         }
     }
 
-    private fun handleClickBtnEvent() {
-        viewModel.clickBtnEvent.observeEvent(viewLifecycleOwner) {
-            startFeedbackPage()
-            NotTodoAmplitude.trackEvent(eventName = getString(R.string.click_ad_modal_cta))
-        }
-    }
-
-    private fun startFeedbackPage() = Intent(
-        Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_feedback_after_use))
+    private fun navigateToOpenChat() = Intent(
+        Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_open_chat))
     ).also { intent -> startActivity(intent) }
 
     override fun bindViewModelWithBinding() {
