30250c15a9783e469e7b6d54549cb64d3e66f44d
giovannijunseokim <kingjunseo@gmail.com>
2024-02-14 00:31:23 +0900

    [feat]#248 이벤트 핸들러 하나로 통합

--- app/src/main/java/kr/co/nottodo/presentation/common/viewmodel/CommonDialogViewModel.kt
+++ app/src/main/java/kr/co/nottodo/presentation/common/viewmodel/CommonDialogViewModel.kt
@@ -3,25 +3,29 @@ package kr.co.nottodo.presentation.common.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kr.co.nottodo.util.Event
-import kr.co.nottodo.util.emit
+import kr.co.nottodo.util.livedata.MutableSingleLiveData
+import kr.co.nottodo.util.livedata.SingleLiveData
 
 class CommonDialogViewModel : ViewModel() {
+
+    private val _event = MutableSingleLiveData<EventType>()
+    val event: SingleLiveData<EventType> = _event
+
     private val _isCheckBoxChecked = MutableLiveData(false)
     val isCheckBoxChecked: LiveData<Boolean> = _isCheckBoxChecked
-    fun changeIsCheckBoxChecked() {
+    fun onCheckBoxClick() {
         _isCheckBoxChecked.value = !(isCheckBoxChecked.value ?: false)
     }
 
-    private val _clickBtnEvent = MutableLiveData<Event<Unit>>()
-    val clickBtnEvent: LiveData<Event<Unit>> = _clickBtnEvent
-    fun onBtnClickEvent() {
-        _clickBtnEvent.emit()
+    fun onBtnClick() {
+        _event.postValue(EventType.NAVIGATE)
     }
 
-    private val _dismissTvClickEvent = MutableLiveData<Event<Unit>>()
-    val dismissTvClickEvent: LiveData<Event<Unit>> = _dismissTvClickEvent
-    fun onDismissTvClickEvent() {
-        _dismissTvClickEvent.emit()
+    fun onDismissTvClick() {
+        _event.postValue(EventType.DISMISS)
+    }
+
+    enum class EventType {
+        NAVIGATE, DISMISS
     }
 }
